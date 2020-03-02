b19650346241c5e24c475796dfedd6a2daee75b1
Gregory Piche <gregory_piche@hotmail.com>
2020-03-01 22:01:18 -0500

    Added comments and improved GUI

--- src/Main.java
+++ src/Main.java
@@ -13,15 +13,14 @@ import javafx.stage.Stage;
 
 public class Main extends Application implements EventHandler<ActionEvent> {
 
+    // GUI component initialization
     public static int count = 2;
     public int[][] chessboard;
     public VBox layout = new VBox(10);
-    public GridPane root = new GridPane();
-    public TextField nInput = new TextField();
+    public GridPane root = new GridPane(); // Where chessboard will be printed
+    public TextField nInput = new TextField(); // Takes user input
     public Scene scene = new Scene(layout, 360, 425);
-
-    // GUI component initialization
-    Button button;
+    public Button button;
 
     public static void main(String[] args) {
         launch(args);
@@ -36,24 +35,27 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         button.setOnAction(this);
 
         Label fieldDescriptor = new Label("Enter value for n:");
-        //TextField nInput = new TextField();
 
-        // Layout
+        // First line layout
         HBox init = new HBox(10);
         init.getChildren().addAll(fieldDescriptor, nInput);
-        layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(init, button);
-        //nInput.setMaxWidth(scene.getWidth() -  fieldDescriptor.getWidth());
-        button.setMaxWidth(scene.getWidth());
         init.setAlignment(Pos.CENTER);
 
-        root.setAlignment(Pos.CENTER);
+        // VBox layout
+        layout.setPadding(new Insets(20, 20, 20, 20));
+        layout.getChildren().addAll(init, button, root);
+        layout.setAlignment(Pos.TOP_CENTER);
 
-        stage.setScene(scene); // align chessboard
+        // Button layout
+        button.setPrefWidth(300);
+
+        // chessboard alignment
+        root.setAlignment(Pos.TOP_CENTER);
+        stage.setScene(scene);
         stage.show();
-
     }
 
+    // handle setups and calls the algorithm
     @Override
     public void handle(ActionEvent event) {
 
@@ -66,14 +68,16 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 
         fill(chessboard, 0, 0, size, 0, size, size);
 
-        /* Uncomment to print to terminal
+
+        /* Uncomment to print 2D array to terminal
         for (int[] row : chessboard){
             System.out.println(Arrays.toString(row));
         }*/
 
-        // Table GUI
-
-        layout.getChildren().remove(root);
+        // Reset constraints on GridPane (root)
+        root.getChildren().clear();
+        root.getColumnConstraints().clear();
+        root.getRowConstraints().clear();
 
         // Set color to chess board triminoes
         for (int row = 0; row < size; row++) {
@@ -122,23 +126,23 @@ public class Main extends Application implements EventHandler<ActionEvent> {
             root.getColumnConstraints().add(new ColumnConstraints(20));
             root.getRowConstraints().add(new RowConstraints(20));
         }
-
-        layout.getChildren().addAll(root);
     }
 
+    // Executes the algo
     public static void fill(int a[][], int rot, int x1, int x2, int y1, int y2, int size){
         int sizex = x2 -1;
         int sizey = y2 - 1;
         int startx = x1;
         int starty = y1;
 
-        if(sizex-x1 == 1){
+        if(sizex-x1 == 1){ // base case of 2x2
             a[starty][startx] = count;
             a[sizey][startx] = count;
             a[sizey][sizex] = count;
             if(rot != 0){
                 rotate(a, rot, x1, x2, y1, y2, size);
             }
+            // The count is used to identify Triminoes in the array/chessboard
             count++;
             if(count == 10){
                 count = 2;
@@ -150,24 +154,21 @@ public class Main extends Application implements EventHandler<ActionEvent> {
             fill(a, 270, x2-((x2-x1)/2), x2, y2-((y2-y1)/2), y2, size); //bottom right
             fill(a, 0, x2-((x2-x1)/2), x2, y1, ((y2-y1)/2)+y1, size); //top right
             if(rot != 0){
-                rotate(a, rot, x1, x2, y1, y2, size);
+                rotate(a, rot, x1, x2, y1, y2, size); // does the rotation of the recursive calls
             }
         }
     }
 
+    // Rotates the triminoes and the recursive calls
     public static void rotate(int a[][], int rot, int x1, int x2, int y1, int y2, int size){
         x2 = x2 - 1;
         y2 = y2 - 1;
-        boolean didRotation = false;
 
-        int[][] newa = new int[size][size];
-        if (rot != 0) {
-            didRotation = true;
-            newa = dorotation(a, rot, x1, x2, y1, y2, size);
-        }
+        int[][] newa;
 
-        if (didRotation) {
-            for (int i = y1; i <= y2; i++) {
+        if (rot != 0) { // Will call dorotation if the angle is not 0
+            newa = dorotation(a, rot, x1, x2, y1, y2, size); // assign rotated array
+            for (int i = y1; i <= y2; i++) { // Transfers the the rotated array to the chessboard array (a)
                 for (int j = x1; j <= x2; j++) {
                     a[i][j] = newa[i][j];
                 }
@@ -175,10 +176,13 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         }
     }
 
+    // this function will rotate the array by desired
     public static int[][] dorotation(int a[][], int rot, int x1, int x2, int y1, int y2, int size){
         int[][] rota = new int[size][size];
 
         int n = x2;
+
+        // Will copy each row of array to right most column of same size array
         for (int r = y1; r <= y2; r++) {
             int m = y1;
             for (int c = x1; c <= x2; c++) {
@@ -187,11 +191,13 @@ public class Main extends Application implements EventHandler<ActionEvent> {
             }
             n--;
         }
-        rot = rot - 90;
-        if (rot != 0){
+
+        rot = rot - 90; // decrement by 90 until rotated desired angle
+
+        if (rot != 0){ // returns rotated array if rotation is complete
             return dorotation(rota, rot, x1, x2, y1, y2, size);
         }
-        return rota;
 
+        return rota; // return rotated array
     }
 }
